27a73c6f465db8a6c57c6ff8a296f1475aa629ea
MoisesCAP <99895424+MoisesCAP@users.noreply.github.com>
2022-08-21 12:26:27 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,15 +15,7 @@ $(function () {
     $(this).find('.dropdown-toggle').removeClass('disabled');
   });
 
-  // Slicknav menu
-  $('.navbar-nav').slicknav({
-      allowParentLinks: true,
-      label: "",
-      appendTo: "#masthead",
-      closedSymbol: '<i class="fa fa-caret-down" aria-hidden="true"></i>',
-      openedSymbol :'<i class="fa fa-caret-up" aria-hidden="true"></i>'
-  });
-
+ 
   // menu click event
   $('.slicknav_btn').click(function() {
     $(this).toggleClass('act');
