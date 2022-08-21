d71d60481b46db125ad0df039906efa807c3601b
MoisesCAP <99895424+MoisesCAP@users.noreply.github.com>
2022-08-21 13:39:10 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,8 +14,16 @@ $(function () {
     $(this).removeClass('open');
     $(this).find('.dropdown-toggle').removeClass('disabled');
   });
+  
+  // Slicknav menu
+  $('.navbar-nav').slicknav({
+    allowParentLinks: true,
+    label: "",
+    appendTo: "#masthead",
+    closedSymbol: '<i class="fa fa-caret-down" aria-hidden="true"></i>',
+    openedSymbol :'<i class="fa fa-caret-up" aria-hidden="true"></i>'
+});
 
- 
   // menu click event
   $('.slicknav_btn').click(function() {
     $(this).toggleClass('act');
